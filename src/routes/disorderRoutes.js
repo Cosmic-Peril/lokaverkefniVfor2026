412004317b2bec40d7db65724c4a8903e7d284a3
Nadia <naida.furtak@gmail.com>
2026-04-10 10:31:03 +0000

    I am lwk chillin

--- src/routes/disorderRoutes.js
+++ src/routes/disorderRoutes.js
@@ -3,3 +3,14 @@ router.get(`/`, disorderController.getHomePage);
 router.get(`/disorder/ny`, disorderController.getAddDisorderForm);
 
 router.get(`/disorder/:id`, disorderController.getDisorderDetails);
+
+
+
+router.get(`/`, disorderController.getHomePage);
+
+router.get(`/disorder/ny`, disorderController.getAddDisorderForm);
+
+router.post(`/disorder/ny`, disorderController.createNewDisorder); 
+
+router.get(`/disorder/:id`, disorderController.getDisorderDetails);
+
